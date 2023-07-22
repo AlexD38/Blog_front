9c578b925102a16f59df4a12f9699911400c52b7
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-07-22 17:25:02 +0200

    added single post page

--- src/components/SinglePost/SinglePost.jsx
+++ src/components/SinglePost/SinglePost.jsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "preact/hooks";
 import "./index.css";
+import data from "../../../data/data.json";
 export default function SinglePost() {
-    return <p>Single post page</p>;
+    const [singlePost, setSinglePost] = useState([]);
+    const [title, setTitle] = useState("");
+    const [slug, setSlug] = useState("");
+    const [body, setBody] = useState("");
+    useEffect(() => {
+        setSinglePost(data[0].posts[0]);
+    }, []);
+    console.log(singlePost);
+    setTitle(singlePost.title);
+    setBody(singlePost.body);
+    setSlug(singlePost.slug);
+    return (
+        <>
+            <div className="single-post-container">
+                <div>
+                    <h1 className="title">{title}</h1>
+                    <p className="slug">{slug}</p>
+                    <div className="body-container">
+                        <p className="body">{body}</p>
+                    </div>
+                </div>
+            </div>
+            <a className="back-home-btn" href="/">
+                <button>Retour à l'accueil</button>
+            </a>
+        </>
+    );
 }
