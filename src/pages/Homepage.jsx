040f967c374de8b7f8ffa73610a537ad7e021a4a
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-08-04 23:11:06 +0200

    added logo and header

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,55 +6,48 @@ import { useState, useRef } from "preact/hooks";
 import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
 
 function HomePage() {
-	const [isAdmin, setIsAdmin] = useState(false);
-	const [loginBtnContent, setLoginBtnContent] = useState("Login");
-	const [showTags, setShowTags] = useState(false);
-	const inputRef = useRef(null);
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [loginBtnContent, setLoginBtnContent] = useState("Login");
+    const [showTags, setShowTags] = useState(false);
+    const inputRef = useRef(null);
 
-	if (localStorage.getItem("isAdmin")) {
-		setIsAdmin(true);
-		setLoginBtnContent("Logout");
-	}
+    if (localStorage.getItem("isAdmin")) {
+        setIsAdmin(true);
+        setLoginBtnContent("Logout");
+    }
 
-	const handleLogout = () => {
-		localStorage.removeItem("isAdmin");
-		setIsAdmin(false);
-		setLoginBtnContent("Login");
-	};
-	const handleShowTags = () => {
-		showTags ? setShowTags(false) : setShowTags(true);
-		console.log(showTags);
-	};
+    const handleLogout = () => {
+        localStorage.removeItem("isAdmin");
+        setIsAdmin(false);
+        setLoginBtnContent("Login");
+    };
+    const handleShowTags = () => {
+        showTags ? setShowTags(false) : setShowTags(true);
+        console.log(showTags);
+    };
 
-	return (
-		<>
-			<LoginBtn
-				isAdmin={isAdmin}
-				onLogout={handleLogout}
-				content={loginBtnContent}
-			/>
-			<div className="taglist-container">
-				{showTags && <TagList isAdmin={isAdmin} />}
-				{showTags ? (
-					<div className="trangle-container">
-						<AiOutlineCaretUp
-							className="triangle"
-							onClick={handleShowTags}
-						/>
-						<p className="show-tags">Hide tags</p>
-					</div>
-				) : (
-					<div className="triangle-container">
-						<AiOutlineCaretDown
-							className="triangle"
-							onClick={handleShowTags}
-						/>
-						<p className="show-tags">Show tags</p>
-					</div>
-				)}
-			</div>
-			<PostsList isAdmin={isAdmin} />
-		</>
-	);
+    return (
+        <>
+            <header className="header">
+                <h1 className="logo">Blog.</h1>
+                <LoginBtn isAdmin={isAdmin} onLogout={handleLogout} content={loginBtnContent} />
+            </header>
+            <div className="taglist-container">
+                {showTags && <TagList isAdmin={isAdmin} />}
+                {showTags ? (
+                    <div className="trangle-container">
+                        <AiOutlineCaretUp className="triangle" onClick={handleShowTags} />
+                        <p className="show-tags">Hide tags</p>
+                    </div>
+                ) : (
+                    <div className="triangle-container">
+                        <AiOutlineCaretDown className="triangle" onClick={handleShowTags} />
+                        <p className="show-tags">Show tags</p>
+                    </div>
+                )}
+            </div>
+            <PostsList isAdmin={isAdmin} />
+        </>
+    );
 }
 export default HomePage;
