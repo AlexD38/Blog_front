85f1fec3048727753d62c6f0ce2dbc3df47781e3
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-08-06 14:33:30 +0200

    search posts ok

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -8,105 +8,96 @@ import { route } from "preact-router";
 import Fuse from "fuse.js";
 
 function PostsList(props) {
-	const [posts, setPosts] = useState([]);
-	const [filteredPosts, setFilteredPosts] = useState([]);
-	const inputRef = useRef(null);
-	const [tagClicked, setTagClicked] = useState(props.tagClicked);
-	const fuseOptions = {
-		keys: ["title"],
-		threshold: 0.2,
-		isCaseSensitive: false,
-	};
-	const fuse = new Fuse(posts, fuseOptions);
-	function searchWithFuse() {
-		const filteredPostsAfterSearch = fuse.search(inputRef.current.value);
-		setFilteredPosts(filteredPostsAfterSearch);
-		console.log(filteredPostsAfterSearch);
-	}
+    const [posts, setPosts] = useState([]);
+    const [filteredPosts, setFilteredPosts] = useState([]);
+    const inputRef = useRef(null);
+    const [tagClicked, setTagClicked] = useState(null);
 
-	useEffect(() => {
-		async function fetchPosts() {
-			try {
-				const response = await axios.get(`http://localhost:4000/posts`);
-				setPosts((PostsList) => response.data);
-				return;
-			} catch (error) {
-				console.log(error);
-			}
-		}
-		fetchPosts();
-	}, [posts]);
+    const fuseOptions = {
+        keys: ["title"],
+        threshold: 0.2,
+        isCaseSensitive: false,
+    };
+    const fuse = new Fuse(posts, fuseOptions);
+    function searchWithFuse() {
+        const filteredPostsAfterSearch = fuse.search(inputRef.current.value);
+        setFilteredPosts(filteredPostsAfterSearch);
+        console.log(filteredPostsAfterSearch);
+    }
 
-	const addPost = () => {
-		route("/addpost");
-	};
-	const handleClick = () => {
-		console.log(props.tagClicked);
-	};
+    useEffect(() => {
+        async function fetchPosts() {
+            try {
+                const response = await axios.get(`http://localhost:4000/posts`);
+                setPosts((PostsList) => response.data);
+                console.log(response.data);
+                return;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchPosts();
+    }, []);
 
-	return (
-		<>
-			<input
-				ref={inputRef}
-				onChange={searchWithFuse}
-				className="input-search-posts"
-				type="search"
-				placeholder="Search for your posts here..."
-				onClick={handleClick}
-			/>
-			{props.isAdmin && (
-				<ul className="post-container">
-					{" "}
-					<li className="add-card">
-						<AiOutlinePlus className="add-icon" onClick={addPost} />
-						WRITE A NEW POST
-					</li>
-				</ul>
-			)}
-			<ul className="post-container">
-				{filteredPosts.length > 0 ? (
-					<>
-						{filteredPosts.map((post) => (
-							<li key={post.item.id} className="card">
-								<h1>{post.item.title}</h1>
-								<h3 className="slug">{post.item.slug}</h3>
-								<a href={"/detailedpost/" + post.item.id}>
-									<button className="readmore">
-										Read More...
-									</button>
-								</a>
-								{props.isAdmin && (
-									<>
-										<DeleteBtn postId={post.id} />
-										<ModifyBtn postId={post.id} />
-									</>
-								)}
-							</li>
-						))}
-					</>
-				) : (
-					<>
-						{posts.map((post) => (
-							<li key={post.id} className="card">
-								<h1>{post.title}</h1>
-								<h3 className="slug">{post.slug}</h3>
-								<a href={"/detailedpost/" + post.id}>
-									<button className="readmore">
-										Read More...
-									</button>
-								</a>
-								{props.isAdmin && (
-									<>
-										<DeleteBtn postId={post.id} />
-										<ModifyBtn postId={post.id} />
-									</>
-								)}
-							</li>
-						))}
-					</>
-				)}
-			</ul>
-		</>
-	);
+    const addPost = () => {
+        route("/addpost");
+    };
+    const handleClick = () => {
+        console.log(props.tagClicked);
+    };
+
+    return (
+        <>
+            <input ref={inputRef} onChange={searchWithFuse} className="input-search-posts" type="search" placeholder="Search for your posts here..." onClick={handleClick} />
+            {props.isAdmin && (
+                <ul className="post-container">
+                    {" "}
+                    <li className="add-card">
+                        <AiOutlinePlus className="add-icon" onClick={addPost} />
+                        WRITE A NEW POST
+                    </li>
+                </ul>
+            )}
+            <ul className="post-container">
+                {filteredPosts.length > 0 ? (
+                    <>
+                        {filteredPosts.map((post) => (
+                            <li key={post.item.id} className="card">
+                                <h1>{post.item.title}</h1>
+                                <h3 className="slug">{post.item.slug}</h3>
+                                <a href={"/detailedpost/" + post.item.id}>
+                                    <button className="readmore">Read More...</button>
+                                </a>
+                                {props.isAdmin && (
+                                    <>
+                                        <DeleteBtn postId={post.id} />
+                                        <ModifyBtn postId={post.id} />
+                                    </>
+                                )}
+                            </li>
+                        ))}
+                    </>
+                ) : (
+                    <>
+                        {posts.map((post) => (
+                            <li key={post.id} className="card">
+                                <h1>{post.title}</h1>
+                                <h3 className="slug">{post.slug}</h3>
+                                <a href={"/detailedpost/" + post.id}>
+                                    <button className="readmore">Read More...</button>
+                                </a>
+                                {props.isAdmin && (
+                                    <>
+                                        <DeleteBtn postId={post.id} />
+                                        <ModifyBtn postId={post.id} />
+                                    </>
+                                )}
+                            </li>
+                        ))}
+                    </>
+                )}
+            </ul>
+        </>
+    );
 }
 export default PostsList;
--- src/components/TagList/TagList.jsx
+++ src/components/TagList/TagList.jsx
@@ -4,102 +4,83 @@ import "./index.css";
 import Fuse from "fuse.js";
 
 function TagList(props) {
-	const [tags, setTags] = useState([]);
-	const [filteredTags, setfilteredTags] = useState([]);
-	const [isAdmin, setIsAdmin] = useState(props.isAdmin);
-	const inputRef = useRef(null);
-	const fuseOptions = {
-		keys: ["name"],
-		threshold: 0.2,
-		isCaseSensitive: false,
-	};
-	const fuse = new Fuse(tags, fuseOptions);
-	useEffect(() => {
-		async function fetchTags() {
-			try {
-				const response = await axios.get("http://localhost:4000/tags");
-				setTags(response.data);
-			} catch (error) {
-				console.log(error);
-			}
-		}
-		fetchTags();
-	}, [tags]);
-	const deleteTag = async (e) => {
-		console.log(`tags number ${e.target.id}`);
-		if (isAdmin) {
-			try {
-				const response = await axios.delete(
-					`http://localhost:4000/tags/${e.target.id}`
-				);
-				// alert(response.data.success);
-			} catch (error) {
-				console.log(error);
-			}
-		}
-	};
-	function searchWithFuse() {
-		const filteredTagsAfterSearch = fuse.search(inputRef.current.value);
-		setfilteredTags(filteredTagsAfterSearch);
-		console.log(filteredTagsAfterSearch);
-	}
-	const handleClick = (e) => {
-		props.clicked(e.target.textContent);
-		console.log(props);
-	};
-	return (
-		<div className="tag-wrapper">
-			<input
-				ref={inputRef}
-				type="text"
-				onChange={searchWithFuse}
-				className="input-search-tags"
-				placeholder="Search for your tags here..."></input>
-			<ul className="tag-container">
-				{filteredTags.length > 0 ? (
-					<>
-						{filteredTags.map((tag) => (
-							<li
-								key={tag.id}
-								id={tag.id}
-								className="tag"
-								onClick={handleClick}>
-								{props.isAdmin && (
-									<button
-										id={tag.id}
-										className="delete-tag-btn"
-										onClick={deleteTag}>
-										X
-									</button>
-								)}
-								{tag.item.name}
-							</li>
-						))}
-					</>
-				) : (
-					<>
-						{tags.map((tag) => (
-							<li
-								key={tag.id}
-								id={tag.id}
-								className="tag"
-								onClick={handleClick}>
-								{props.isAdmin && (
-									<button
-										id={tag.id}
-										className="delete-tag-btn"
-										onClick={deleteTag}>
-										X
-									</button>
-								)}
-								{tag.name}
-							</li>
-						))}
-					</>
-				)}
-			</ul>
-		</div>
-	);
+    const [tags, setTags] = useState([]);
+    const [filteredTags, setfilteredTags] = useState([]);
+    const [isAdmin, setIsAdmin] = useState(props.isAdmin);
+    const inputRef = useRef(null);
+
+    const fuseOptions = {
+        keys: ["name"],
+        threshold: 0.2,
+        isCaseSensitive: false,
+    };
+    const fuse = new Fuse(tags, fuseOptions);
+    useEffect(() => {
+        async function fetchTags() {
+            try {
+                const response = await axios.get("http://localhost:4000/tags");
+                setTags(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchTags();
+    }, []);
+    const deleteTag = async (e) => {
+        console.log(`tags number ${e.target.id}`);
+        if (isAdmin) {
+            try {
+                const response = await axios.delete(`http://localhost:4000/tags/${e.target.id}`);
+                // alert(response.data.success);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    };
+    function searchWithFuse() {
+        const filteredTagsAfterSearch = fuse.search(inputRef.current.value);
+        setfilteredTags(filteredTagsAfterSearch);
+        console.log(filteredTagsAfterSearch);
+    }
+    const handleClick = (e) => {
+        props.clicked(e.target.textContent);
+        console.log(props);
+        // console.log(e.target.textContent);
+    };
+    return (
+        <div className="tag-wrapper">
+            <input ref={inputRef} type="text" onChange={searchWithFuse} className="input-search-tags" placeholder="Search for your tags here..."></input>
+            <ul className="tag-container">
+                {filteredTags.length > 0 ? (
+                    <>
+                        {filteredTags.map((tag) => (
+                            <li key={tag.id} id={tag.id} className="tag" onClick={handleClick}>
+                                {props.isAdmin && (
+                                    <button id={tag.id} className="delete-tag-btn" onClick={deleteTag}>
+                                        X
+                                    </button>
+                                )}
+                                {tag.item.name}
+                            </li>
+                        ))}
+                    </>
+                ) : (
+                    <>
+                        {tags.map((tag) => (
+                            <li key={tag.id} id={tag.id} className="tag" onClick={handleClick}>
+                                {props.isAdmin && (
+                                    <button id={tag.id} className="delete-tag-btn" onClick={deleteTag}>
+                                        X
+                                    </button>
+                                )}
+                                {tag.name}
+                            </li>
+                        ))}
+                    </>
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default TagList;
